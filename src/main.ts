5e9a862816834864349c62641d3e9de1f859075f
Christopher Robin Chase <christopherchase011@gmail.com>
2024-12-10 12:25:10 +0800

    update main.ts removed element

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,10 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import 'element-plus/dist/index.css'
 
 import App from './App.vue'
 import router from './router'
 import { useErrorsStore } from './stores/errors'
-import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 
 const app = createApp(App)
@@ -37,10 +35,6 @@ app.config.errorHandler = (err, instance, info) => {
         component: () => import('./404.vue'),
         meta: { layout: 'ErrorLayout' },
     })
-}
-
-for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
-    app.component(key, component)
 }
 
 app.use(createPinia().use(piniaPluginPersistedstate))
